2eabe07ebb3ef17a284a52bc33632da621ddee51
Gert <buysgert@belgacom.net>
2016-12-10 12:05:30 +0100

    Ionic Week3 - Einde oefening displaying loading message during state transition

--- conFusion/www/js/app.js
+++ conFusion/www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $ionicLoading) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,7 +18,24 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
-    }
+    };
+
+    $rootScope.$on('loading:show', function() {
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner>Wachten...'
+      })
+    });
+    $rootScope.$on('loading:hide', function() {
+      $ionicLoading.hide();
+    });
+    $rootScope.$on('$stateChangeStart', function() {
+      console.log('Loading...');
+      $rootScope.$broadcast('loading:show');
+    });
+    $rootScope.$on('$stateChangeSuccess', function() {
+      console.log('State change done');
+      $rootScope.$broadcast('loading:hide');
+    })
   });
 })
 
