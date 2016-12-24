ab4f9ca0c86d2e6b78286039bcf1a4269f3213ff
Gert <buysgert@belgacom.net>
2016-12-24 15:12:42 +0100

    Ionic Week4 - oef registration

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage, $ionicPlatform, $cordovaCamera) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -11,6 +11,8 @@ angular.module('conFusion.controllers', [])
 
   // Form data for the login modal
   $scope.loginData = $localStorage.getObject('userinfo','{}');
+
+  $scope.registration = {};
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -65,11 +67,63 @@ angular.module('conFusion.controllers', [])
     }, 1000);
   };
 
+  // Create the registration modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/register.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.registerform = modal;
+  });
 
+  // Triggered in the registration modal to close it
+  $scope.closeRegister = function () {
+    $scope.registerform.hide();
+  };
+
+  // Open the registration modal
+  $scope.register = function () {
+    $scope.registerform.show();
+  };
+
+  // Perform the registration action when the user submits the registration form
+  $scope.doRegister = function () {
+    console.log('Doing reservation', $scope.reservation);
+    // Simulate a registration delay. Remove this and replace with your registration
+    // code if using a registration system
+    $timeout(function () {
+      $scope.closeRegister();
+    }, 1000);
+  };
+
+  $ionicPlatform.ready(function() {
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+    $scope.takePicture = function() {
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.registration.imgSrc = "data:image/jpeg;base64," + imageData;
+      }, function(err) {
+        console.log(err);
+      });
+
+      $scope.registerform.show();
+
+    };
+  });
 })
 
 
-  .controller('MenuController', ['$scope', 'dishes', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function($scope, dishes, favoriteFactory, baseURL, $ionicListDelegate) {
+  .controller('MenuController', ['$scope', 'dishes', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPlatform',
+    '$cordovaLocalNotification', '$cordovaToast',
+    function($scope, dishes, favoriteFactory, baseURL, $ionicListDelegate, $ionicPlatform,
+    $cordovaLocalNotification, $cordovaToast) {
 
     $scope.baseURL = baseURL;
     $scope.tab = 1;
@@ -111,6 +165,20 @@ angular.module('conFusion.controllers', [])
 
       favoriteFactory.addToFavorites(index);
       $ionicListDelegate.closeOptionButtons();
+      $ionicPlatform.ready(function() {
+        $cordovaLocalNotification.schedule({
+          id:1,
+          title: "Added favorite",
+          text: $scope.dishes[index].name
+        }).then(function() {
+          console.log('Added Favorite' + $scope.dishes[index].name);
+        },
+        function() {
+          console.log('Failed to add favorite');
+        });
+      });
+      $cordovaToast.show('Added favorite ' + $scope.dishes[index].name, 'long', 'center').
+        then(function(success) {}, function (error) {});
     }
   }])
   .controller('ContactController', ['$scope', function($scope) {
@@ -277,7 +345,7 @@ angular.module('conFusion.controllers', [])
          console.log('favorites' + typeof favorites[i].id);
         for (var j = 0; j < dishes.length; j++) {
            console.log('dingen' + typeof dishes[j].id);
-          if (dishes[j].id === favorites[i].id)
+          if (parseInt(dishes[j].id) === parseInt(favorites[i].id))
             out.push(dishes[j]);
         }
       }
